fd077fcdec145f798db7883131bb57fdf0558ce3
Adam Power <31990347+adampower48@users.noreply.github.com>
2018-11-25 03:29:41 +0000

    Fixed DATA field parsing.

--- PC-AXIS File reader.py
+++ PC-AXIS File reader.py
@@ -14,9 +14,24 @@ class PxFieldDialect(csv.Dialect):
     quotechar = '"'
     lineterminator = '\n'
 
+class PxDataDialect(csv.Dialect):
+    strict = True
+    skipinitialspace = True
+    quoting = csv.QUOTE_ALL
+    delimiter = ' '
+    quotechar = '"'
+    lineterminator = '\n'
+
+    
 # Parse csv-like string
-def parse_px_field(field):
-    r = csv.reader(StringIO(field), PxFieldDialect())
+def parse_px_field(field, _type=None):
+    # _type: "field", "data"
+    dialects = {
+        "field": PxFieldDialect,
+        "data": PxDataDialect,
+    }
+    
+    r = csv.reader(StringIO(field), dialects.get(_type, dialects["field"])())
     
     out = []
     for line in r:
@@ -46,27 +61,27 @@ def read_px(filename):
 
 
     head = field_dict["HEADING"].strip("\"")
-    headers = parse_px_field(field_dict["VALUES(\"{}\")".format(head)])
+    headers = parse_px_field(field_dict["VALUES(\"{}\")".format(head)], _type="field")
 
 
     # ## Find & Parse MultiIndex levels
     # * Find headers
 
 
-    levels = parse_px_field(field_dict["STUB"])
+    levels = parse_px_field(field_dict["STUB"], _type="field")
 
 
     # * Parse values
 
 
-    level_values = [parse_px_field(field_dict["VALUES(\"{}\")".format(lev)]) for lev in levels]
+    level_values = [parse_px_field(field_dict["VALUES(\"{}\")".format(lev)], _type="field") for lev in levels]
     levels_dict = dict(zip(levels, level_values))
 
 
     # ## Parse Data
     # * Split into stream of cells
 
-    data = field_dict["DATA"].replace("\n", "").split()
+    data = parse_px_field(field_dict["DATA"], _type="data")
 
 
     # * Convert into table
@@ -88,6 +103,6 @@ def read_px(filename):
     # * Flatten MultiIndex
     # * Convert all cell dtypes
 
-    df2 = df.reset_index().convert_objects(convert_numeric=True)
+    df2 = df.reset_index().convert_objects()
 
     return df2
